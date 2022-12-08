44ca8edf765604a1f973c93cfe3af97350fe3165
seshadrireddy10 <seshadrireddy1492@gmail.com>
2022-12-08 19:27:51 +0530

    pipeline branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("welcome to develop branch...................!");
+        System.out.println("welcome to devops pipeline........................................................................!");
     }
 }
